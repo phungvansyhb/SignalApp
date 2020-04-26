6d3da99963299b36c8cfb9931f7cf7f3f410a383
phungvansyhb <54164578+phungvansyhb@users.noreply.github.com>
2020-04-26 15:47:47 +0700

    Updates

---
+++ precache-manifest.8f9f4f33f0ec49bc8833d3379127cd31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64525ddc8a0baabd4ac1ef0f788cec6",
+    "url": "/SigniApp/index.html"
+  },
+  {
+    "revision": "c257182ed351d1617223",
+    "url": "/SigniApp/static/css/main.a6252d81.chunk.css"
+  },
+  {
+    "revision": "a0cf1390007ce73bb9a7",
+    "url": "/SigniApp/static/js/2.f3cccb10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SigniApp/static/js/2.f3cccb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c257182ed351d1617223",
+    "url": "/SigniApp/static/js/main.3fc34ab4.chunk.js"
+  },
+  {
+    "revision": "d66cb7531997f8a037ec",
+    "url": "/SigniApp/static/js/runtime-main.b8a4c6f2.js"
+  },
+  {
+    "revision": "704dd166206832f862936a88aef1e654",
+    "url": "/SigniApp/static/media/bg.704dd166.jpg"
+  }
+]);
